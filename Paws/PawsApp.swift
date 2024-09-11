e0bd1cacd3bb15c2fa94303900aca5b7676d18ec
debbelzz <A00835550@tec.mx>
2024-09-11 10:56:27 -0600

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Debanhi Medina on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
