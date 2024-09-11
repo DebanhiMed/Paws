42fdc15d91274c3c8cf17f76b75b60cc99df2b87
IsidroTrevino <A00837104@tec.mx>
2024-09-11 15:31:07 -0600

    Finished Sign In page and added test view for navigation

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Sign_In()
-            
         }
         .padding()
     }
--- Paws/Sign In.swift
+++ Paws/Sign In.swift
@@ -11,93 +11,96 @@ struct Sign_In: View {
     @State private var email = ""
     @State private var password = ""
     
+    @State private var testEmail = "irdo@tec.mx"
+    @State private var testPassword = "Debanhiminobia"
+    
     var body: some View {
-        VStack(alignment: .center) {
-            Text("Paws")
-                .font(.system(size: 50))
-                .fontWeight(.bold)
-                .fontDesign(Font.Design.rounded)
-            
-            Image("pawIcon")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 126, height: 126)
-            
-            Text("Sign in")
-                .font(.title)
-                .fontDesign(Font.Design.rounded)
-                .padding(.top)
+        NavigationView {
+            VStack(alignment: .center) {
+                Text("Paws")
+                    .font(.system(size: 50))
+                    .fontWeight(.bold)
+                    .fontDesign(Font.Design.rounded)
+                
+                Image("pawIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 126, height: 126)
+                
+                Text("Sign in")
+                    .font(.title)
+                    .fontDesign(Font.Design.rounded)
+                    .padding(.top)
 
-            Form {
-                VStack(alignment: .leading, spacing: 15) {
-                    Text("Email")
-                        .font(.headline)
-                        .foregroundColor(.black)
-                    
-                    TextField("Email", text: $email)
-                        .padding()
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                        .keyboardType(.emailAddress)
-                        .autocapitalization(.none)
-                    
-                    Text("Password")
-                        .font(.headline)
-                        .foregroundColor(.black)
-                    
-                    SecureField("Password", text: $password)
-                        .padding()
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                    
-                    Button(action: {
-                        print("Sign In button tapped")
-                    }) {
-                        Text("Sign In")
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                    .padding(.top, 10)
-                    
-                    Button(action: {
-                        print("Forgot Password tapped")
-                    }) {
-                        Text("Forgot password?")
-                            .font(.footnote)
+                Form {
+                    VStack(alignment: .leading, spacing: 15) {
+                        Text("Email")
+                            .font(.headline)
                             .foregroundColor(.black)
-                            .underline()
-                    }
-                    .padding(.top, 10)
-                }
-                .frame(width: 300)
-                .padding(20)
-                .background(Color.white)
-                .cornerRadius(20)
-            .shadow(radius: 10)
-            }
-            .formStyle(.columns)
-            
-            Spacer()
-            
-            HStack {
-                Button(action: {
-                    print("Create account button tapped")
-                        }) {
-                    Text("Don't have an account?")
-                        .font(.footnote)
-            
-                                Text("Create one.")
-                               }
-                        .font(.footnote)
-                        .foregroundStyle(Color.black)
-                Image(systemName: "arrow.right")
-                    .padding(.leading, 40)
+                        
+                        TextField("Email", text: $email)
+                            .padding()
+                            .background(Color(.systemGray6))
+                            .cornerRadius(10)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        
+                        Text("Password")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                        
+                        SecureField("Password", text: $password)
+                            .padding()
+                            .background(Color(.systemGray6))
+                            .cornerRadius(10)
+                        
+                        NavigationLink(destination: Test()) {
+                            Text("Sign In")
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
                         }
-                   }
-                   .padding(.horizontal)
+                        .padding(.top, 10)
+                        
+                        Button(action: {
+                            print("Forgot Password tapped")
+                        }) {
+                            Text("Forgot password?")
+                                .font(.footnote)
+                                .foregroundColor(.black)
+                                .underline()
+                        }
+                        .padding(.top, 10)
+                    }
+                    .frame(width: 300)
+                    .padding(20)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
+                }
+                .formStyle(.columns)
+                
+                Spacer()
+                
+                HStack {
+                    Button(action: {
+                        print("Create account button tapped")
+                    }) {
+                        Text("Don't have an account?")
+                            .font(.footnote)
+                        Text("Create one.")
+                    }
+                    .font(.footnote)
+                    .foregroundStyle(Color.black)
+                    
+                    Image(systemName: "arrow.right")
+                        .padding(.leading, 40)
+                }
+            }
+            .padding(.horizontal)
+        }
         Spacer()
     }
 }
